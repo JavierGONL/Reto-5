be573d7b502d2e9145775de5e897715de9b8288d
JavierGONL <JavierGONL@users.noreply.github.com>
2025-06-28 14:58:32 -0500

    modified:   shape_package/__init__.py

--- shape_package/__init__.py
+++ shape_package/__init__.py
@@ -1,14 +1,5 @@
-# Paquete Shape - Reto 5
-# Este paquete contiene todas las clases relacionadas con figuras geométricas
-
-# ENFOQUE 1: Un módulo único dentro del paquete Shape
-# Importa todas las clases desde un solo módulo
-from .all_shapes_module import *
-
-# ENFOQUE 2: Módulos individuales que importan Shape e inheritan de él
-# Comentar las líneas de arriba y descomentar las de abajo para usar este enfoque
-# from .shape_module import Shape
-# from .point_module import Point
-# from .line_module import Line
-# from .rectangle_module import Rectangle
-# from .triangle_module import Triangle, Isosceles, Equilateral, Scalene, TriRectangle
+from .shape_module import Shape
+from .point_module import Point
+from .line_module import Line
+from .rectangle_module import Rectangle
+from .triangle_module import Triangle, Isosceles, Equilateral, Scalene, TriRectangle
